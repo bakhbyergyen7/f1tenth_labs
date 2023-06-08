49b5a35c35c408b32a103fb1262af6730a58b50f
Bakhbyergyen Yerjan <bakhbyergyen7@gmail.com>
2023-06-08 16:14:49 -0500

    TTC calculated

--- src/lab2_safety/scripts/lab2_safety.py
+++ src/lab2_safety/scripts/lab2_safety.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 
 import numpy as np
 # TODO: include needed ROS msg type headers and libraries
+from std_msgs.msg import Bool
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
@@ -28,7 +29,18 @@ class SafetyNode(Node):
         self.publisher = self.create_publisher(AckermannDriveStamped, '/drive', 10)
         self.subscriber_lidar = self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
         self.subscriber_odom = self.create_subscription(Odometry, '/ego_racecar/odom', self.odom_callback, 10)
+        
+        self.THRESHOLD = 0.4
+        self.ranges = []
+        self.angles = []
+        self.angles_cos = []
+        self.angle_min = 0
+        self.angle_max = 0
+        self.angle_increment = 0
         self.speed = 0.
+
+        self.initialized = False
+        
         self.get_logger().info('Safety Node initialized')
  
     def odom_callback(self, odom_msg):
@@ -39,7 +51,27 @@ class SafetyNode(Node):
         # TODO: calculate TTC
         ranges = np.array(scan_msg.ranges)
         # TODO: publish command to brake
-        pass
+        # Initialization: read off values from the sensor
+        if self.initialized == False:
+            self.angle_min = scan_msg.angle_min
+            self.angle_max = scan_msg.angle_max
+            self.angle_increment = scan_msg.angle_increment
+            self.angles = np.linspace(self.angle_min, self.angle_max, len(self.ranges))
+            self.angles_cos = np.cos(self.angles)
+            self.initialized = True
+        self.calculate_TTC()
+        # TTC Calculation
+    def calculate_TTC(self):
+        if self.speed < 0.1 and self.speed > -0.1:
+            return
+        V_x = self.angles_cos * self.speed
+        TTCs = np.divide(self.ranges, V_x)
+        lower_than_threshold = False
+        for TTC in TTCs:
+            if TTC > 0 and TTC < self.THRESHOLD:
+                lower_than_threshold = True
+                break
+        if lower_than_threshold:
 
 def main(args=None):
     rclpy.init(args=args)
